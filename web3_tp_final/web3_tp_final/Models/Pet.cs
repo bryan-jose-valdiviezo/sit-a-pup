b679d4b055047056b71098c18c3544b9b43f27b4
Jade <jtraverselheureux@dept-info.quebec>
2021-10-29 15:19:09 -0400

    Création des classes dans le model.

---
+++ web3_tp_final/web3_tp_final/Models/Pet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web3_tp_final.Models
+{
+    public class Pet
+    {
+        
+        public int PetId { get; set; }
+
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+        
+        [Required]
+        public Specie Specie { get; set; }
+        
+        
+        public int Age { get; set; }
+
+        public string PhotoURI { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+    }
+}
---
+++ web3_tp_final/web3_tp_final/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web3_tp_final.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        [StringLength(20)]
+        [Required]
+        [MinLength(3),MaxLength(20)]
+        public string UserName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
+        public string Email { get; set; }
+
+        [Required]
+        public String Address { get; set; }
+
+        public List<PhoneNumber> PhoneNumbers { get; set; }
+
+        public List<Pet> OwnedPets { get; set; }
+
+        public List<Pet> SittedPets { get; set; }
+
+        public List<Availability> Availabilities { get; set; }
+
+        public List<Review> MyReviews { get; set; }
+
+        public List<Review> ReviewsOtherUsers { get; set; }
+
+        public List<Message> MessagesSent { get; set; }
+    }
+}
