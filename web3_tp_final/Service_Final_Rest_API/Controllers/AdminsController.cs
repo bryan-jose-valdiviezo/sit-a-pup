2ad69e8d28706990b47d8b2389b5e5abb55782bd
Jade <jtraverselheureux@dept-info.quebec>
2021-11-19 10:51:00 -0500

    Ajout du Controller Admin dans l'API

---
+++ web3_tp_final/Service_Final_Rest_API/Controllers/AdminsController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Service_Final_Rest_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Service_Final_Rest_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminsController : Controller
+    {
+        private readonly sitapupContext _context;
+
+        public AdminsController(sitapupContext context)
+        {
+
+            _context = context;
+
+            // GET: api/Admins
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Admin>>> GetAdmins()
+        {
+            return await _context.Admins.ToListAsync();
+        }
+
+        // GET: api/Admins/16
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Admin>> GetAdmin(long id)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            return admin;
+        }
+
+        // PUT: api/Admins/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAdmin(long id, Admin admin)
+        {
+            if (id != admin.AdminId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(admin).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdminExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Admins
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Admin>> PostPet(Admin admin)
+        {
+            _context.Admins.Add(admin);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAdmin", new { id = admin.AdminId }, admin);
+        }
+
+        // DELETE: api/Admins/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAdmin(long id)
+        {
+            var admin = await _context.Admins.FindAsync(id);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            _context.Admins.Remove(admin);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AdminExists(long id)
+        {
+            return _context.Admins.Any(e => e.AdminId == id);
+        }
+    }
+}
