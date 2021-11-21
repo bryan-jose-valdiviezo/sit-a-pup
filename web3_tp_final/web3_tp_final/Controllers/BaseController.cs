2474f03fe2545021113a18fae59963b5cb139eb0
bvaldiviezohuamani <bvaldiviezohuamani@dept-info.crosemont.quebec>
2021-11-21 18:00:18 -0500

    Added Base Controller

---
+++ web3_tp_final/web3_tp_final/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web3_tp_final.API;
+using web3_tp_final.Helpers;
+using web3_tp_final.Models;
+
+namespace web3_tp_final.Controllers
+{
+    public class BaseController : Controller
+    {
+        private APIController api;
+        public User CurrentUser()
+        {
+            return SessionHelper.GetObjectFromJson<User>(HttpContext.Session, "user");
+        }
+
+        public APIController Api()
+        {
+            return api;
+        }
+    }
+}
