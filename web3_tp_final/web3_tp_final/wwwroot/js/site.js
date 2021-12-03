b905330659f08e9fb1dfad09398e74bafa5ad2fc
bvaldiviezohuamani <bvaldiviezohuamani@dept-info.crosemont.quebec>
2021-12-03 14:24:29 -0500

    Fixed background for website

--- web3_tp_final/web3_tp_final/wwwroot/js/site.js
+++ web3_tp_final/web3_tp_final/wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿function GetSpinner() {
-    return "<div class='spinner-border' role='status'>" +
+    return "<div class='spinner-border spinner-border-sm' role='status'>" +
             "<span class='sr-only'> Loading...</span>" +
             "</div>"
 }
