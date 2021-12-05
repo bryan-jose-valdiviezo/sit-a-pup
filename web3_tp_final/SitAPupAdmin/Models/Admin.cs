33eafd4692fda60c141c8ed1fc45c4b741513473
Jade <jtraverselheureux@dept-info.quebec>
2021-12-04 20:27:27 -0500

    Delete commentaires.

--- web3_tp_final/SitAPupAdmin/Models/Admin.cs
+++ web3_tp_final/SitAPupAdmin/Models/Admin.cs
@@ -9,13 +9,9 @@ namespace SitAPupAdmin.Models
     public class Admin
     {
         public int AdminID { get; set; }
-        [Display(Name = "Nom d'utilisateur")]
-        [Required(ErrorMessage = "Minimum 3 caractères, maximum 20 caractères")]
-        [MinLength(3), MaxLength(20)]
+       
         public string Name { get; set; }
-        [Display(Name = "Mot de passe")]
-        [Required(ErrorMessage = "Minimum 6 caractères, maximum 32 caractères")]
-        [MinLength(3), MaxLength(20)]
+       
         public string Password { get; set; }
     }
 }
